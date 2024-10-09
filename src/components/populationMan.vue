171adcbaf5273774767f10e0ed792d70d7bc0251
bfl-dev <paparrasilva2@gmail.com>
2024-10-09 14:56:10 -0300

    Initial commit

---
+++ src/components/populationMan.vue
@@ -0,0 +1,26 @@
+﻿<script setup>
+
+import {onMounted, ref} from 'vue';
+import axios from 'axios';
+
+let worldData = ref([]);
+let algo = worldData.value
+
+const getPopulation = async () => {
+  await axios.get("https://countriesnow.space/api/v0.1/countries/population")
+      .then( req => req.data.data.forEach( d => worldData.value.push(d)))
+}
+
+onMounted( () => {
+  getPopulation();
+  console.log(worldData.value.keys)
+  worldData.value.forEach( r => console.log(r))
+})
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
